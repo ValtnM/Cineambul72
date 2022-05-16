b3824cae3c088b0997e816582f1e3ba21e54407f
Valentin <valentin.monteiro@orange.fr>
2022-05-16 13:00:43 +0200

    Ajustements responsive

--- frontend/src/Components/Commune/Commune.js
+++ frontend/src/Components/Commune/Commune.js
@@ -7,17 +7,16 @@ export default function Commune(props) {
       <div className='commune'>
         <h3>{props.commune.nom}</h3>
         <div className="commune-salle">
-          <img src={props.commune.photo} alt={`Salle de ${props.commune.nom}`} />
-          <div className="salle-infos">
+          {/* <img src={props.commune.photo} alt={`Salle de ${props.commune.nom}`} /> */}
+          {/* <div className="commune-salle-infos"> */}
             <h4>{props.commune.salle.nom}</h4>
             <p>{props.commune.salle.rue}</p>
             <p>{props.commune.salle.commune}</p>
             <p>{props.commune.salle.contact}</p>
-          </div>
+          {/* </div> */}
         </div>
         <Slider dataSlider={props.commune.salle.photos} />
         <div className="commune-seances">
-          {/* <hr /> */}
           <h4>Films à l'affiche</h4>
           <div className='commune-seances__films'>
             <img src="/affiches/bruno-reidal.jpg" alt="" />
