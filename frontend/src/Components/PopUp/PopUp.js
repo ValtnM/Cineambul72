dee4b0dcd82284b06341a5cfc08b77907bd4cace
Valentin <valentin.monteiro@orange.fr>
2022-09-08 21:36:37 +0200

    Création d'un loading spinner au chargement de la bande annonce

--- frontend/src/Components/PopUp/PopUp.js
+++ frontend/src/Components/PopUp/PopUp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PopUp.scss'
 import ReactPlayer from 'react-player'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,14 +6,28 @@ import { faCircleXmark } from '@fortawesome/free-regular-svg-icons'
 
 export default function PopUp(props) {
 
+  
+  const [trailerReady, setTrailerReady] = useState(false)
+  
+  useEffect(() => {
+    console.log(trailerReady);
+  }, [trailerReady])
     const closePopUp = () => {
         props.setPopUpTrailer(false)
     }
 
   return (
-    <div className='pop-up'>        
-        <ReactPlayer className="pop-up-trailer" url={props.trailerUrl} controls></ReactPlayer>
-        <FontAwesomeIcon onClick={() => closePopUp()} className='pop-up-btn' icon={faCircleXmark}></FontAwesomeIcon>        
-    </div>
+    <div className='pop-up'> 
+        
+            
+            <ReactPlayer className="pop-up-trailer" style={trailerReady ? {display: "block"} : {display: "none"}} onReady={() => setTrailerReady(true)} url={props.trailerUrl} controls></ReactPlayer>
+            <FontAwesomeIcon onClick={() => closePopUp()} className='pop-up-btn' icon={faCircleXmark}></FontAwesomeIcon>        
+          
+          
+          <div className='loader' style={!trailerReady ? {display: "block"} : {display: "none"}} >
+            
+          </div>
+            
+          </div>
   )
 }
