456455203cd3a3a766674465c44015cfd75f288d
Valentin <valentin.monteiro@orange.fr>
2022-10-05 17:23:03 +0200

    Ajout d'une checkbox pour afficher le mot de passe sur le formulaire de connexion

--- frontend/src/Components/ConnectionForm/ConnectionForm.js
+++ frontend/src/Components/ConnectionForm/ConnectionForm.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./ConnectionForm.scss"
 
 export default function ConnectionForm(props) {
+
+  const [showPassword, setShowPassword] = useState(false);  
   
   // Envoi des identifiants de connexion
   const submitConnectionData = (e) => {
@@ -13,10 +15,19 @@ export default function ConnectionForm(props) {
     <div className='connection-form'>
         <h2>Connexion administrateur</h2>
         <form>
-            <label htmlFor="">Identifiant</label>
-            <input onChange={(e) => props.setUserName(e.target.value)} type="text" />
-            <label htmlFor="">Mot de passe</label>
-            <input onChange={(e) => props.setPassword(e.target.value)} type="password" autoComplete='off'/>
+            <label htmlFor="username">Identifiant</label>
+            <input onChange={(e) => props.setUserName(e.target.value)} type="text" id='username' />
+            <label htmlFor="password">Mot de passe</label>
+            {
+              showPassword ?
+              <input onChange={(e) => props.setPassword(e.target.value)} type="text" id='password' autoComplete='off'/>
+              :
+              <input onChange={(e) => props.setPassword(e.target.value)} type="password" autoComplete='off' id='password'/>
+            }
+            <div className="password-checkbox">
+              <label htmlFor="display-password">Afficher le mot de passe</label>
+              <input onChange={(e) => setShowPassword(e.target.checked)} type="checkbox" id="display-password"/>
+            </div>
             <button onClick={(e) => submitConnectionData(e)}>Se connecter</button>
         </form>
     </div>
