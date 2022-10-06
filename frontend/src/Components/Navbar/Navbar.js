fcc7397e802d581f8e4437f8a52f4db9a0cbad71
Valentin <valentin.monteiro@orange.fr>
2022-10-06 11:57:29 +0200

    Ajustements CSS

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -22,7 +22,8 @@ export default function Navbar() {
     }
 
     useEffect(() => {
-        setPageName(pageUrl)            
+        setPageName(pageUrl)  
+        setToggleMenu(false)          
     }, [pageUrl])
     
     useEffect(() => {
